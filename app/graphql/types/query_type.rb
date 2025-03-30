87a6d00bf73d3629618148f2542c0609717344ac
Igor Zubkov <igor.zubkov@gmail.com>
2025-03-30 20:07:27 +0200

    Update

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -8,6 +8,8 @@ module Types
 
     field :corporation, resolver: Resolvers::Eve::CorporationResolver
 
+    field :character, resolver: Resolvers::Eve::CharacterResolver
+
     field :node, Types::NodeType, null: true, description: "Fetches an object given its ID." do
       argument :id, ID, required: true, description: "ID of the object."
     end
